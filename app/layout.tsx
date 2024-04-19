79d67ef8fcab3050a1eb6d21e5e63906f67c6a73
Mahmoud ELshora <67161371+elshora@users.noreply.github.com>
2024-04-19 15:57:36 +0200

    update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,24 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
-
+// app/Layout.tsx
 export const metadata: Metadata = {
   title: "Study Race",
   description: "Study Race Project under development",
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/hat.svg", // Path to light mode favicon
+        href: "/hat.svg",
+      },
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/hat.svg", // Path to dark mode favicon
+        href: "/hat.svg",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
